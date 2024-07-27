39a75d95cbb0f11d9452ede9e3f06f346e0f1ec4
John Dunn <johnwdunn20@gmail.com>
2024-07-27 16:56:36 -0700

    added page header

--- client/app/(tabs)/history.tsx
+++ client/app/(tabs)/history.tsx
@@ -11,8 +11,7 @@ export default function History() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/barbell.png')}
         />
       }>
       <ThemedView style={styles.titleContainer}>
