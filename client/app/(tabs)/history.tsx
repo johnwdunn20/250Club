c030b7b7055f185c43c03b858a0281b5c288f461
John Dunn <johnwdunn20@gmail.com>
2024-07-27 19:08:23 -0700

    succesfully getting server data

--- client/app/(tabs)/history.tsx
+++ client/app/(tabs)/history.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { API_URL } from '@/config';
 
 interface ServerResponse {
   message: string;
