4a427c4e5b10575af0f7e6c235d5020e2e0692eb
John Dunn <johnwdunn20@gmail.com>
2024-08-17 16:10:36 -0700

    getting data from db

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,70 +1,65 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { API_URL } from '@/lib/config';
+import { Image, StyleSheet, Platform } from "react-native";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useSession } from "@/hooks/useSession";
+import { supabase } from "@/lib/supabase";
 
 interface ServerResponse {
   message: string;
 }
 
-const fetchHistory = async () => {
-  console.log(`URL: ', ${API_URL}/api/test`);
-  const response = await fetch(`${API_URL}/api/test`);
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  const data =  await response.json();
-
-  return data;
-};
-
 export default function History() {
+  const { session } = useSession();
+
+  console.log("session", session);
+
+  const fetchHistory = async () => {
+    const { data, error } = await supabase.from("exercises").select("*");
+    // Need to revisit schema and figure out exactly what I want
+
+    console.log("data", data);
+    return data;
+  };
 
   const queryClient = useQueryClient();
 
-  const {data, isError, isLoading} = useQuery({
-    queryKey: ['history'],
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["history"],
     queryFn: fetchHistory,
   });
 
-
-  return ( 
+  return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/barbell.png')}
+          source={require("@/assets/images/barbell.png")}
           style={styles.headerImage}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">250 Club History</ThemedText>
       </ThemedView>
 
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">You play to win the game </ThemedText>
-        <ThemedText>
-          {`History coming soon - ${
-            isLoading
-              ? 'Loading...'
-              : isError
-              ? 'Error loading history'
-              : data?.message
-            }`
-          }
-        </ThemedText>
+        {data &&
+          data.map((exercise: any) => (
+            <ThemedText key={exercise.id}>{exercise.name}</ThemedText>
+          ))}
+        <ThemedText></ThemedText>
       </ThemedView>
-
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -72,8 +67,8 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   headerImage: {
-    height: '100%',
-    width: '100%',
-    resizeMode: 'cover',
+    height: "100%",
+    width: "100%",
+    resizeMode: "cover",
   },
 });
