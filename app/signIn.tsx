801887955c56cb888651e57dff55e1350f5f2301
John Dunn <johnwdunn20@gmail.com>
2024-08-17 15:18:27 -0700

    redirect to home if already signed in

--- app/signIn.tsx
+++ app/signIn.tsx
@@ -1,8 +1,25 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 import Auth from "@/components/Auth";
+import { useSession } from "@/hooks/useSession";
+import { Redirect } from "expo-router";
 
 export default function SignIn() {
+
+  const { session, isLoading } = useSession();
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (session) {
+    return <Redirect href="/" />;
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <Auth />
--- components/Auth.tsx
+++ components/Auth.tsx
@@ -4,6 +4,7 @@ import { supabase } from "../lib/supabase";
 import { Button, Input } from "@rneui/themed";
 import { useForm } from "react-hook-form";
 
+
 // Tells Supabase Auth to continuously refresh the session automatically if
 // the app is in the foreground. When this is added, you will continue to receive
 // `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
